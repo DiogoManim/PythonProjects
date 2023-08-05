85631107c94cba37e3a9ad26a295e8d5d1dc22ea
diogo manim <diogomanim@ua.pt>
2023-08-05 22:34:54 +0100

    Commit and Push. First project - Band Name Generator. Final version.

---
+++ Project 1/bandNameGenerator.py
@@ -0,0 +1,7 @@
+print("Hi! This is a BAND NAME GENERATOR! Let's find out a creative name for yours.")
+
+city = input("What is the name of the city that you grew up in?\n")
+
+pet = input("What was the name of your first pet?\n")
+
+print("Your brand name is " + city + " " + pet + "!")
