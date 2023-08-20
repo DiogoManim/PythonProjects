dd7e5632ad252fdc5a11ba28cc104eaf8158d613
diogo manim <diogomanim@ua.pt>
2023-08-20 02:34:50 +0100

    Update - Creating Routes/Views (TO FIX)

--- FLASK WEB APP/main.py
+++ FLASK WEB APP/main.py
@@ -1,6 +1,7 @@
 from website import create_app
 
-app = crete_app()
+app = create_app()
 
-if __name__ == '__main__':  # 
-    app.run(debug=True)
+if __name__ == '__main__':  # This makes the server run only when the file is executed.
+    app.run(debug=True)     # Every time we make a change in our code, it's going to automatically re-run the web server.
+                            # If the debug was False, we would have to re-run manually the code to update the server.
--- FLASK WEB APP/website/__init__.py
+++ FLASK WEB APP/website/__init__.py
@@ -7,6 +7,12 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)   # __name__ represents the name of the file
     app.config['SECRET_KEY'] = 'k!as@koiw9i34ad12Alasdw2 21H&'   # This is going to encrypt/secure the cookies/session data, related to the website
+    
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
 
     return app
 
